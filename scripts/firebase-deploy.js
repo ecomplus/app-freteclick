41669d508efa1564e72fe6d40ead4136bda3bd9d
Vitor <vitorrggm@gmail.com>
2026-05-21 19:50:22 -0300

    fix(deploy): enable legacyRuntimeConfigCommands for firebase-tools v15 compatibility

--- scripts/firebase-deploy.js
+++ scripts/firebase-deploy.js
@@ -12,37 +12,28 @@ require('./scripts-minification')
 
 const { name, version } = require('../package.json')
 const { project, baseUri } = require('./_constants')
-const client = require('firebase-tools')
 
-const config = [
+const firebase = './node_modules/.bin/firebase'
+
+const configValues = [
   `pkg.version=${version}`,
   `pkg.name=${name}`,
   `server.operator_token=${SERVER_OPERATOR_TOKEN}`
 ]
 if (SERVER_BASE_URI) {
-  config.push(`server.base_uri=${SERVER_BASE_URI}`)
+  configValues.push(`server.base_uri=${SERVER_BASE_URI}`)
 }
 
 console.log(`Deploying to Firebase project: '${project}'`)
 
-client.functions.config.set(config, { project })
-  .then(() => {
-    execSync(
-      `./node_modules/.bin/firebase deploy --project ${project} --force`,
-      { stdio: 'inherit' }
-    )
-    console.log(
-      '\x1b[32m%s\x1b[0m',
-      `\nDeployed with success to Firebase project '${project}'`
-    )
-    console.log(
-      '\x1b[35m%s\x1b[0m',
-      `\nBase URI: ${baseUri}`
-    )
-    console.log()
-  })
-
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
+try {
+  execSync(`${firebase} experiments:enable legacyRuntimeConfigCommands`, { stdio: 'inherit' })
+  execSync(`${firebase} functions:config:set ${configValues.join(' ')} --project ${project}`, { stdio: 'inherit' })
+  execSync(`${firebase} deploy --project ${project} --force`, { stdio: 'inherit' })
+  console.log('\x1b[32m%s\x1b[0m', `\nDeployed with success to Firebase project '${project}'`)
+  console.log('\x1b[35m%s\x1b[0m', `\nBase URI: ${baseUri}`)
+  console.log()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
